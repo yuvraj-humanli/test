3dcf105a1a3f9ca9c08e2c604775239ec2e9ee1d
Yuvraj <yuvraj.dagur@humanli.ai>
2024-05-24 17:56:55 +0530

    Learning: Used get request in our project to fetch random quotes

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -8,39 +8,21 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class QuotesComponent implements OnInit{
   quote = "";
-  spends = [];
-  total = 0;
   constructor(
     private data: DataService
   ) { }
 
   ngOnInit(): void {
-      // this.data.getData().subscribe({
-      //   next: (res: any) => {
-      //     this.quote = res.content;
-      //     console.log(res);
+      this.data.getData().subscribe({
+        next: (res: any) => {
+          this.quote = res.content;
+          console.log(res);
           
-      //   }, error: (err: any) => {
-      //     console.log("Error", err);
+        }, error: (err: any) => {
+          console.log("Error", err);
           
-      //   }
-      // })
-  }
-
-  dateSelect(date:any){
-    const newDate = new Date(date);
-    date = (newDate.getMonth()+1)+'/'+newDate.getDate()+'/'+newDate.getFullYear();
-    this.data.getSpends(date).subscribe({
-      next : (res:any) => {
-        this.total = res.data;
-        this.spends = res.sources;
-        
-      },error: (err: any) => {
-        console.log("Error", err);
-        
-      }
-    })
-    
+        }
+      })    
   }
   
 
